97435064ee431d983fc700c4c86a7790076cb82c
Midubo <47749932+Midubo@users.noreply.github.com>
2020-06-27 19:04:35 +0300

    Add files via upload

--- Pet/UserControls/UserControl_MainMenu.xaml.cs
+++ Pet/UserControls/UserControl_MainMenu.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Pet.Properties;
+using System;
 using System.Diagnostics;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
 
 namespace Pet
 {
@@ -13,46 +16,80 @@ namespace Pet
     {
         void IDisposable.Dispose() { }
 
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+
         public UserControl_MainMenu()
         {
             InitializeComponent();
 
             Version version = Assembly.GetExecutingAssembly().GetName().Version;
             tblc_version.Text += string.Format(" - Version {0}.{1}.{2}", version.Major, version.Minor, version.Build);
+
+            mediaPlayer.Open(new Uri(string.Format("{0}\\Robot_Boogie.mp3", AppDomain.CurrentDomain.BaseDirectory)));
+            mediaPlayer.MediaEnded += new EventHandler(Media_Ended);
+
+            if ((bool)Settings.Default["music"] == true)
+            {
+                mediaPlayer.Play();
+            }
+        }
+
+        private void Media_Ended(object sender, EventArgs e)
+        {
+            if ((bool)Settings.Default["music"] == true)
+            {
+                mediaPlayer.Position = TimeSpan.Zero;
+                mediaPlayer.Play();
+            }
         }
 
         private void btn_Play_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            this.Content = new UserControl_SelectPet();
+            mediaPlayer.Stop();
+            (Parent as Window).Content = new UserControl_SelectPet();
         }
 
         private void btn_Options_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            Window_Options wo = new Window_Options();
-            wo.ShowDialog();
-        }
+            this.Opacity = 0.4;
+            this.Effect = new BlurEffect();
 
-        private void btn_Creators_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            if (border_Creators.Visibility == Visibility.Visible)
+            Window_Options wo = new Window_Options()
             {
-                border_Creators.Visibility = Visibility.Collapsed;
+                Owner = this.Parent as Window,
+                ShowInTaskbar = false
+            };
+            wo.ShowDialog();
+
+            this.Opacity = 1;
+            this.Effect = null;
+            (Parent as Window).ShowInTaskbar = true;
+
+            if ((bool)Settings.Default["music"] == false)
+            {
+                mediaPlayer.Stop();
             }
-            else border_Creators.Visibility = Visibility.Visible;
+            else
+            {
+                mediaPlayer.Position = TimeSpan.Zero;
+                mediaPlayer.Play();
+            }
+
+            if ((bool)Settings.Default["windowed"] == false)
+            {
+                (Parent as Window).WindowStyle = WindowStyle.None;
+                (Parent as Window).WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                (Parent as Window).WindowStyle = WindowStyle.ToolWindow;
+                (Parent as Window).WindowState = WindowState.Maximized;
+            }
         }
 
         private void btn_Exit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
-        }
-
-        private void btn_Version_Click(object sender, RoutedEventArgs e)
-        {
-            if (tblc_version.Visibility == Visibility.Visible)
-            {
-                tblc_version.Visibility = Visibility.Collapsed;
-            }
-            else tblc_version.Visibility = Visibility.Visible;
         }
 
         private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
@@ -63,7 +100,7 @@ namespace Pet
 
         private void btn_Website_Click(object sender, RoutedEventArgs e)
         {
-
+            Process.Start(new ProcessStartInfo(link.NavigateUri.AbsoluteUri));
         }
     }
 }
