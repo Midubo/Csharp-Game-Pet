a146472d687de2f6dd7021e50d9bf4bd8b3aa3ff
Midubo <47749932+Midubo@users.noreply.github.com>
2020-09-01 16:16:04 +0300

    17. Final Window

--- Pet/Windows/Window_Final.xaml.cs
+++ Pet/Windows/Window_Final.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Pet.Properties;
+using System.Windows;
+using System.Windows.Media;
 
 namespace Pet
 {
@@ -10,6 +12,29 @@ namespace Pet
         public Window_Final(int happiness)
         {
             InitializeComponent();
+
+            tblc_Final.Text = happiness.ToString();
+
+            if (happiness >= 100)
+            {
+                Settings.Default["wins"] = (int)Settings.Default["wins"] + 1;
+                Settings.Default.Save();
+
+                tblc_result.Text = "You won!";
+                tblc_comment.Text = "Excellent!";
+            }
+            else
+            {
+                header.Background = Brushes.Coral;
+
+                tblc_result.Text = "You lost";
+                tblc_comment.Text = "Your pet is sad.";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
